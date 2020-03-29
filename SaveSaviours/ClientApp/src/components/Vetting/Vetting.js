894435f53ed2935479c8b2241d9aa3e91f9cd6b1
Tom Lawson <tomlawson@toms-mbp.speedport.ip>
2020-03-29 04:32:32 +0200

    vetting page frontend

--- SaveSaviours/ClientApp/src/components/Vetting/Vetting.js
+++ SaveSaviours/ClientApp/src/components/Vetting/Vetting.js
@@ -1,30 +1,25 @@
-import React, { useState } from 'react'
-import { Paper, Typography, Checkbox } from '@material-ui/core'
+import React from 'react'
+import {
+  Paper, Typography, Checkbox,
+} from '@material-ui/core'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
-import TablePagination from '@material-ui/core/TablePagination'
 import TableRow from '@material-ui/core/TableRow'
-import TableSortLabel from '@material-ui/core/TableSortLabel'
 import * as messages from 'messages/de.json'
 import styles from 'styles/styles'
 
-const Vetting = () => {
+
+const Vetting = ({ vetted, setVetted, unvettetInstitutions }) => {
   const classes = styles()
-  const [vetted, setVetted] = useState()
-  // get list of unvetted institution registrations from backend
-  const unvettetInstitutions = [
-    {
-      contactName: 'Peter Lustig',
-      institutionName: 'DRK',
-      landline: '',
-      mobile: '017722222222',
-      email: 'peter@lustig.com',
-      zip: '55555',
-    },
-  ]
+  const handleCheckbox = (event) => {
+    setVetted({
+      ...vetted,
+      [event.target.name]: { verified: !vetted[event.target.name].verified, indeterminate: false },
+    })
+  }
   return (
     <Paper square elevation={2} className={classes.landingPaper}>
       <Typography variant="h1" className={classes.landingHeader}>
@@ -32,23 +27,42 @@ const Vetting = () => {
       </Typography>
       <TableContainer component={Paper}>
         <Table>
-          <TableHead dense>
-            <TableBody>
-              {unvettetInstitutions.map((institution) => (
+          <TableHead>
+            <TableRow>
+              <TableCell>{messages['registrationpage.institution.institutionName']}</TableCell>
+              <TableCell>{messages['registrationpage.institution.contactName']}</TableCell>
+              <TableCell>{messages['registrationpage.institution.email']}</TableCell>
+              <TableCell>{messages['registrationpage.institution.landline']}</TableCell>
+              <TableCell>{messages['registrationpage.institution.mobile']}</TableCell>
+              <TableCell>{messages['registrationpage.institution.zip']}</TableCell>
+              <TableCell>{messages['vettingpage.institution.timestamp']}</TableCell>
+              <TableCell>{messages['vettingpage.institution.vetted']}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {unvettetInstitutions.map((institution) => (
+              vetted[institution.email]
+                && (
                 <TableRow key={institution.email}>
-                  <TableCell className={classes.landingText}>
-                    <Checkbox />
-                  </TableCell>
                   <TableCell>{institution.institutionName}</TableCell>
                   <TableCell>{institution.contactName}</TableCell>
                   <TableCell>{institution.email}</TableCell>
                   <TableCell>{institution.landline}</TableCell>
                   <TableCell>{institution.mobile}</TableCell>
                   <TableCell>{institution.zip}</TableCell>
+                  <TableCell>{institution.timestamp}</TableCell>
+                  <TableCell className={classes.landingText}>
+                    <Checkbox
+                      checked={vetted[institution.email].verified}
+                      indeterminate={vetted[institution.email].indeterminate}
+                      onChange={(event) => handleCheckbox(event)}
+                      name={institution.email}
+                    />
+                  </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </TableHead>
+                )
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </Paper>

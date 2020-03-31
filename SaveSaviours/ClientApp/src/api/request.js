cc8c1ed33c1684bfa9d1047c08972341800139b4
Tom Lawson <tomlawson@toms-mbp.speedport.ip>
2020-03-31 04:17:04 +0200

    request bug fixed and experimenting with signin and registration

--- SaveSaviours/ClientApp/src/api/request.js
+++ SaveSaviours/ClientApp/src/api/request.js
@@ -1,9 +1,9 @@
-const HOST = process.env.REACT_APP_BACKEND_HOST
+
 
 export default (path, method, data = undefined) => {
   const auth = window.localStorage.getItem('access-token')
 
-  return fetch(`${HOST}${path}`, {
+  return fetch(`https://savesaviours.azurewebsites.net/api${path}`, {
     method,
     headers: {
       'Content-Type': 'application/json',
